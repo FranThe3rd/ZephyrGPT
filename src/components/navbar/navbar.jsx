0156379aad7ed99526e29d173598d4362da8148a
Francisco <figueroaf@moravian.edu>
2024-05-16 19:44:58 -0400

    fixed and finished media queries for the home header section, reponsive navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,8 +1,66 @@
 import React from 'react'
 import './navbar.css'
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { List } from 'phosphor-react'
+import { InstagramLogo } from 'phosphor-react'
+import { ShoppingCart } from 'phosphor-react'
 
 const Navbar = () => {
+
+    const [showNavbar, setShowNavbar] = useState(false)
+
+
+    const handleShowNavbar = () => {
+        setShowNavbar(!showNavbar)
+    }
   return (
+    <nav className="navbar">
+            <div className="container-nav">
+                <div className="logo">
+                    <h1>ZephyrGPT</h1>
+                    
+                </div>
+                <div className="menu-icon" onClick={handleShowNavbar}>
+                    <List size={40} color='white' />
+                </div>
+                <div className={`nav-elements  ${showNavbar && 'active'}`}>
+                    <ul>
+                        <li>
+                            <NavLink onClick={handleShowNavbar} to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink onClick={handleShowNavbar} to="/shop">OpenAI</NavLink>
+                        </li>
+                        <li>
+                            <NavLink onClick={handleShowNavbar} to="/contact">Case Studies</NavLink>
+                        </li>
+                        <li>
+                        <NavLink onClick={handleShowNavbar} to="/contact">Library</NavLink>
+
+                        </li>
+                        <li>
+                        <NavLink onClick={handleShowNavbar} to="/contact">Live Demo</NavLink>
+
+                            
+                        </li>
+                        <li>
+                        <NavLink onClick={handleShowNavbar} to="/contact">Sign In</NavLink>
+
+                            
+                        </li>
+                        <li>
+                        <NavLink className='nav-sign' onClick={handleShowNavbar} to="/contact">Sign Up</NavLink>
+
+                            
+                        </li>
+                    
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    /** 
     <div className='navbar'>
       <li>
         <a className='nav-title' href="">ZephyrGPT</a>
@@ -12,14 +70,17 @@ const Navbar = () => {
         <a href="">Case Studies</a>
         <a href="">Library</a>
       </li>
+      <a href="#" className='toggle-button'>
+        <span className='bar'></span>
+        <span className='bar'></span>
+        <span className='bar'></span>
+      </a>
       <li>
         <a href="">Sign In</a>
         <a className='nav-sign' href="">Sign Up</a>
       </li>
-
-
-
     </div>
+    */
   )
 }
 
