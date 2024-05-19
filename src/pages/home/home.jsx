2693f05c2c847305e09ceb09ec037e821d99dff4
Francisco <figueroaf@moravian.edu>
2024-05-18 23:24:49 -0400

    added new section in the home.jsx which gives detail about how the chatbot works, and added simple css hover effect for navbar

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,6 +3,7 @@ import './home.css'
 import Navbar from '../../components/navbar/navbar'
 import ai from '../../assets/ai.png'
 import people from '../../assets/people.png'
+import { InstagramLogo, TwitterLogo, DiscordLogo, FacebookLogo } from 'phosphor-react'
 
 const Home = () => {
   return (
@@ -22,9 +23,32 @@ const Home = () => {
         <div className='home-header-img'>
           <img src={ai} alt="" />
         </div>
-
-
       </div>
+      <div className='home-icons'>
+        <InstagramLogo size={30}  />
+        <TwitterLogo size={30}  />
+        <DiscordLogo size={30}  />
+        <FacebookLogo size={30}  />
+      </div>
+      <div className='home-what'>
+        <h1>What is ZephyrGPT?</h1>
+        <h3>ZephyrGPT is an AI-powered chatbot that delivers unmatched intelligence and personalized responses, adapting seamlessly to your style. Enjoy secure, engaging conversations anytime with its user-friendly interface. Experience the future of AI interactions with ZephyrGPT.</h3>
+        <div className='home-what-grid'>
+          <div className='home-what-card'>
+            <h4>Chatbots</h4>
+            <p>ZephyrGPT's chatbot provides intelligent, context-aware conversations for personalized and efficient user interactions.</p>
+          </div>
+          <div className='home-what-card'>
+            <h4>Knowledgebase</h4>
+            <p>ZephyrGPT's knowledgebase is a powerful tool that provides instant answers to your questions, making it easier to find the information you need.</p>
+          </div>
+          <div className='home-what-card'>
+            <h4>Education</h4>
+            <p>ZephyrGPT's educational resources help you learn new skills and gain knowledge on a wide range of topics, from programming to marketing.</p>
+          </div>
+        </div>
+      </div>
+
 
     </div>
   )
