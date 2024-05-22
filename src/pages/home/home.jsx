d663f25f40a17c45a771efd9df628b9f0ebd4948
Francisco <figueroaf@moravian.edu>
2024-05-22 19:50:15 -0400

    added blog section

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,6 +5,13 @@ import ai from '../../assets/ai.png'
 import people from '../../assets/people.png'
 import { InstagramLogo, TwitterLogo, DiscordLogo, FacebookLogo } from 'phosphor-react'
 
+import mainImg from '../../assets/main.png'
+import sub1Img from '../../assets/sub1.png'
+import sub2Img from '../../assets/sub2.png'
+import sub3Img from '../../assets/sub3.png'
+import sub4Img from '../../assets/sub4.png'
+
+
 const Home = () => {
   return (
     <div className='home'>
@@ -73,6 +80,64 @@ const Home = () => {
           <p>Explore the diverse applications of AI in everyday tasks, from personal assistance to smart home automation, and see how it can transform your routine.</p>
           <p>Learn about ChatGPT as a reliable and friendly digital assistant, ready to help with information, entertainment, and support whenever you need it.</p>
           <p>Understand the role of AI in modern communication, enhancing clarity, understanding, and efficiency in both personal and professional interactions.</p>
+        </div>
+      </div>
+      <div className='home-register'>
+        <div className='home-register-text'>
+          <p>Request Early Access and Get Early Access to ZephyrGPT</p>
+          <h1>Join the Future of AI Conversations</h1>
+        </div>
+        <button>Get Started</button>
+      </div>
+      <div className='home-blog'>
+        <h1> Alot is happening, </h1>
+        <h1>We are blogging about it</h1>
+        <div className='home-blog-grid'>
+          <div className='home-blog-main'>
+            <img src={mainImg} alt="" />
+            <div className='home-blog-main-overlay'>
+              <h3>May 22, 2024</h3>
+              <p>Discover how ZephyrGPT is transforming the way we interact, offering intelligent, context-aware responses that enhance communication.</p>
+              <h5>Read Full Article</h5>
+            </div>
+          </div>
+
+
+          <div className='home-blog-sub1'>
+            <img src={sub1Img} alt="" />
+            <div className='home-blog-sub1-overlay'>
+              <h3>May 24, 2024</h3>
+              <p>The Future of Virtual Assistants: Exploring ZephyrGPT’s Cutting-Edge Features</p>
+              <h5>Read Full Article</h5>
+            </div>
+
+          </div>
+
+          <div className='home-blog-sub2'>
+            <img src={sub2Img} alt="" />
+            <div className='home-blog-sub2-overlay'>
+              <h3>May 22, 2024</h3>
+              <p>Harnessing the Wind: How ZephyrGPT Revolutionizes AI Conversations</p>
+              <h5>Read Full Article</h5>
+            </div>
+          </div>
+          <div className='home-blog-sub3'>
+            <img src={sub3Img} alt="" />
+            <div className='home-blog-sub3-overlay'>
+              <h3>May 22, 2024</h3>
+              <p>From Concept to Reality: The Development Journey of ZephyrGPT</p>
+              <h5>Read Full Article</h5>
+            </div>
+          </div>
+          <div className='home-blog-sub4'>
+            <img src={sub4Img} alt="" />
+            <div className='home-blog-sub4-overlay'>
+              <h3>May 22, 2024</h3>
+              <p>Enhancing User Experience with ZephyrGPT: Tips and Best Practices
+              </p>
+              <h5>Read Full Article</h5>
+            </div>
+          </div>
 
 
         </div>
